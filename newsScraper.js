93ee02868cbda1ac8a628861c7cf7bb42185ec86
Evgenii-Dolgopolov <do.evgenii@gmail.com>
2024-06-21 12:37:50 +0700

    change timeout settings

--- newsScraper.js
+++ newsScraper.js
@@ -22,11 +22,16 @@ const newsScraper = async (
     headless: chromium.headless,
     executablePath: process.env.CHROMIUM_PATH,
     ignoreHTTPSErrors: true,
-    protocolTimeout: 100000
+    // protocolTimeout: 100000
   })
 
   // Create a new page
   const page = await browser.newPage()
+
+  // Set specific timeouts for actions
+  page.setDefaultNavigationTimeout(100000) // Navigation timeout
+  page.setDefaultTimeout(100000) // Overall page timeout
+
   // Set user agent to prevent detection
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -111,7 +116,9 @@ const newsScraper = async (
           // console.log(`Article with URL ${article.url} has no content. Skipping insertion.`);
         }
       } catch (error) {
-        console.error(`Error navigating to ${article.url} because ${error.message}`)
+        console.error(
+          `Error navigating to ${article.url} because ${error.message}`
+        )
       }
     }
   } catch (error) {
